b1ddac4f138a7c9ae8a59acd6cade9e6e93b016c
Eray Erdin <eraygezer.94@gmail.com>
2023-02-07 10:33:23 +0300

    revert to deprecated backgroundColor

--- lib/context_extensions/theme_extension.dart
+++ lib/context_extensions/theme_extension.dart
@@ -22,7 +22,7 @@ extension ThemeExt on BuildContext {
   // COLOR
 
   /// performs a simple [Theme.of(context).backgroundColor] action and returns given [backgroundColor]
-  Color get backgroundColor => Theme.of(this).colorScheme.background;
+  Color get backgroundColor => Theme.of(this).backgroundColor;
 
   /// performs a simple [Theme.of(context).primaryColor] action and returns given [primaryColor]
   Color get primaryColor => Theme.of(this).primaryColor;
