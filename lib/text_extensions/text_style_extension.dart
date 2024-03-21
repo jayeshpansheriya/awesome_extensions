28cf3776068cdf4ce160b573f5367ade32645d65
Altynbek Aidarbekov <altynbek.ktl@gmail.com>
2024-03-22 01:04:18 +0500

    feat(text-extensions): refactor font weight extension methods
    Added extension methods to easily apply different font weights to a TextStyle in the TxtStyle extension on TextStyle in text_style_extension.dart. Each method now uses the copyWith method to update the font weight property.
    
    (cherry picked from commit 28d5d39a31fd79a4f5805dc668e2c1ec2c7bc1df)

--- lib/text_extensions/text_style_extension.dart
+++ lib/text_extensions/text_style_extension.dart
@@ -2,38 +2,38 @@ part of '../awesome_extensions.dart';
 
 extension TxtStyle on TextStyle {
   /// The most thick - FontWeight.w900
-  TextStyle get mostThick => TextStyle(
-      fontSize: fontSize, fontFamily: fontFamily, fontWeight: FontWeight.w900);
+  TextStyle get mostThick => copyWith(fontWeight: FontWeight.w900);
+  TextStyle get w900 => copyWith(fontWeight: FontWeight.w900);
 
   /// Extra-bold - FontWeight.w800
-  TextStyle get extraBold => TextStyle(
-      fontSize: fontSize, fontFamily: fontFamily, fontWeight: FontWeight.w800);
+  TextStyle get extraBold => copyWith(fontWeight: FontWeight.w800);
+  TextStyle get w800 => copyWith(fontWeight: FontWeight.w800);
 
   /// Bold - FontWeight.bold - FontWeight.w700
-  TextStyle get bold => TextStyle(
-      fontSize: fontSize, fontFamily: fontFamily, fontWeight: FontWeight.w700);
+  TextStyle get bold => copyWith(fontWeight: FontWeight.w700);
+  TextStyle get w700 => copyWith(fontWeight: FontWeight.w700);
 
   /// Semi-bold - FontWeight.w600
-  TextStyle get semiBold => TextStyle(
-      fontSize: fontSize, fontFamily: fontFamily, fontWeight: FontWeight.w600);
+  TextStyle get semiBold => copyWith(fontWeight: FontWeight.w600);
+  TextStyle get w600 => copyWith(fontWeight: FontWeight.w600);
 
   /// Medium - FontWeight.w500
-  TextStyle get medium => TextStyle(
-      fontSize: fontSize, fontFamily: fontFamily, fontWeight: FontWeight.w500);
+  TextStyle get medium => copyWith(fontWeight: FontWeight.w500);
+  TextStyle get w500 => copyWith(fontWeight: FontWeight.w500);
 
   /// Regular - FontWeight.w400
-  TextStyle get regular => TextStyle(
-      fontSize: fontSize, fontFamily: fontFamily, fontWeight: FontWeight.w400);
+  TextStyle get regular => copyWith(fontWeight: FontWeight.w400);
+  TextStyle get w400 => copyWith(fontWeight: FontWeight.w400);
 
   /// Light - FontWeight.w300
-  TextStyle get light => TextStyle(
-      fontSize: fontSize, fontFamily: fontFamily, fontWeight: FontWeight.w300);
+  TextStyle get light => copyWith(fontWeight: FontWeight.w300);
+  TextStyle get w300 => copyWith(fontWeight: FontWeight.w300);
 
   /// Extra-light - FontWeight.w200
-  TextStyle get extraLight => TextStyle(
-      fontSize: fontSize, fontFamily: fontFamily, fontWeight: FontWeight.w200);
+  TextStyle get extraLight => copyWith(fontWeight: FontWeight.w200);
+  TextStyle get w200 => copyWith(fontWeight: FontWeight.w200);
 
   /// Thin, the least thick - FontWeight.w100
-  TextStyle get thin => TextStyle(
-      fontSize: fontSize, fontFamily: fontFamily, fontWeight: FontWeight.w100);
+  TextStyle get thin => copyWith(fontWeight: FontWeight.w100);
+  TextStyle get w100 => copyWith(fontWeight: FontWeight.w100);
 }
