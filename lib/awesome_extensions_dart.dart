445a0a3e39375f0b8bfdaafb6b7ab8530af4092f
Altynbek Aidarbekov <altynbek.ktl@gmail.com>
2024-03-22 00:38:36 +0500

    feat: dart and flutter export libs

---
+++ lib/awesome_extensions_dart.dart
@@ -0,0 +1,4 @@
+export 'src/dart/async_snapshot_extension.dart';
+export 'src/dart/date_extensions/date_extension.dart';
+export 'src/dart/number_extension.dart';
+export 'src/dart/string_extension.dart';
